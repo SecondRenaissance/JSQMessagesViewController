7a8aadc73f2a231d6c2f0e406d470186fe883634
JustZht <zhtsu47@me.com>
2018-02-16 02:27:01 +0800

    Use Inset

--- JSQMessagesViewController/Views/JSQMessagesComposerTextView.m
+++ JSQMessagesViewController/Views/JSQMessagesComposerTextView.m
@@ -156,8 +156,7 @@
 
     if ([self.text length] == 0 && self.placeHolder) {
         [self.placeHolderTextColor set];
-
-        [self.placeHolder drawInRect:CGRectInset(rect, 7.0f, 5.0f)
+        [self.placeHolder drawInRect:CGRectInset(rect, 6 + self.textContainerInset.left + self.contentInset.left, self.textContainerInset.top + self.contentInset.top)
                       withAttributes:[self jsq_placeholderTextAttributes]];
     }
 }
